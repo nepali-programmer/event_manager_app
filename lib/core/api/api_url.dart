f8c6ca64d65a423596312342ad49ad1232c1210b
Sijan Joshi <94091594+nepali-programmer@users.noreply.github.com>
2023-03-26 15:24:44 +0545

    updated

--- lib/core/api/api_url.dart
+++ lib/core/api/api_url.dart
@@ -3,8 +3,8 @@ import 'package:injectable/injectable.dart';
 @LazySingleton()
 class ApiUrl {
   // use this on emulator
-  // final String _baseUrl = 'http://10.0.2.2:8000';
-  final String _baseUrl = 'http://192.168.1.4:8000';
+  final String _baseUrl = 'http://10.0.2.2:8000';
+  // final String _baseUrl = 'http://192.168.1.4:8000';
   String get login => '$_baseUrl/api/token';
   String get ticketType => '$_baseUrl/ticket-type';
   String get event => '$_baseUrl/api/event';
